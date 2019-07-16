f620fdb35864d25c90b8468595532c20121cb84d
windect <rbghksdl14@gmail.com>
2019-07-16 15:30:42 +0900

    first problem

---
+++ problem1.js
@@ -0,0 +1,23 @@
+const { Api, JsonRpc, RpcError } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
+const { PrivateKey, PublicKey, Signature, Aes, key_utils, config } = require('eosjs-ecc');
+const { TextEncoder, TextDecoder } = require('util');
+const fetch = require('node-fetch');
+
+let privateKeys = [];// user private keys
+const rpc = new JsonRpc('http://192.168.1.75:8010', { fetch });
+const signatureProvider = new JsSignatureProvider(privateKeys);
+const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+async function main() {
+    try {
+        const result = await rpc.get_block(300000);
+        console.log(result.producer)
+        console.log(result.timestamp)
+    } catch (error) {
+       console.errore(error); 
+        
+    }
+
+}
+main();
